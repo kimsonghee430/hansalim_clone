63643f378bdf8a23a044c9900476a79936318d06
kimsonghee430 <bokchi8@gmail.com>
2024-09-13 15:33:04 +0900

    비쥬얼 슬라이드 영역 js 설정 완료

--- js/script.js
+++ js/script.js
@@ -1,74 +1,82 @@
 window.addEventListener("load", function () {
-  const header = this.document.querySelector(".header");
-  //    console.log(header);
-  let scy = 0;
-  console.log(scy);
+  // data.json을 로딩, 연결시킨다.
+  const xhttp = new XMLHttpRequest();
+  //    console.log(xhttp);
+  xhttp.onreadystatechange = function (e) {
+    const req = e.target;
+    // console.log(req);
 
-  this.window.addEventListener("scroll", function () {
-    // console.log(scy);
-    scy = this.document.documentElement.scrollTop;
-    // console.log(scy);
-    if (scy > 0) {
-      header.classList.add("active");
-    } else {
-      header.classList.remove("active");
+    if (req.readyState === XMLHttpRequest.DONE) {
+      const str = req.response;
+      console.log(str);
+      //   json문자열로 변화 JSON.parse(str)
+      let obj = JSON.parse(str);
+      //   console.log(obj);
+      VISUAL_ARR = obj.visual;
+      //   오늘의 물품
+
+      //   console.log(VISUAL_ARR);
+      // ================
+      showVisual(); // 비주얼을 화면에 배치
+      //오늘의 물품 화면에 배치
     }
-  });
-  //  펼침목록 보기 기능
-  // 더보기 목록기능
-  const menuBt = document.getElementById("menu-bt");
-  const menuList = document.getElementById("menu-list");
-  // 참여 목록기능
-  const joinBt = document.getElementById("join-bt");
-  const joinList = document.getElementById("join-list");
-  //  조합원센터 목록기능
-  const centerBt = document.getElementById("center-bt");
-  const centerList = document.getElementById("center-list");
-  // 배열 순서번호가 주어진다.
-  const toggleListArr = [menuList, joinList, centerList];
-  const toggleBtArr = [menuBt, joinBt, centerBt];
-  // 펼침 목록 모두 닫기
-
-  // 코드 블럭이 같은 기능을 반복된다
-  // 기능을 만들어서 사용(함수)
-
-  function listToggle(bt, list) {
-    // console.log(bt);
-    // console.log(list);
-    list.style.display = "none";
-    bt.addEventListener("click", function () {
-      // 전체 클릭했을때 기능을 초기화
-      toggleBtArr.forEach(function (item) {
-        // console.log(item);
-        item.classList.remove("active");
-      });
-      const nowListId = list.getAttribute("id");
-      console.log(nowListId);
-      const hideArr = toggleListArr.filter(function (item) {
-        // console.log(item);
-        let id = item.getAttribute("id");
-        console.log(id);
-        if (id !== nowListId) {
-          return this;
-        }
-      });
-      //   해당하는 버튼의 리스트 제외한 배열재생
-      //   console.log(hideArr);
-      hideArr.forEach(function (item) {
-        // console.log(item);
-        item.style.display = "none";
-      });
-      const css = getComputedStyle(list).display;
-      if (css === "none") {
-        list.style.display = "block";
-        bt.classList.add("active");
-      } else {
-        list.style.display = "none";
-        bt.classList.remove("active");
-      }
+  };
+  //   자료호출
+  xhttp.open("GET", "data.json");
+  xhttp.send();
+  //   ============================================
+  // 비주얼 슬라이드==
+  let VISUAL_ARR;
+  let visualTag = this.document.getElementById("data-visual");
+  //   오늘의 물품
+  // ==============================================
+  // 비주얼 화면 출력 기능
+  function showVisual() {
+    let html = "";
+    VISUAL_ARR.forEach(function (item) {
+      //   console.log(item);
+      const tag = `
+    <div class="swiper-slide">
+                <div class="visual-slide-page">
+                  <a href="${item.link}">
+                    <img src="images/${item.pic}" alt="${item.name}">
+                  </a>
+                </div>
+               </div>
+    `;
+      html += tag;
     });
-  }
-  listToggle(menuBt, menuList);
-  listToggle(joinBt, joinList);
-  listToggle(centerBt, centerList);
+    visualTag.innerHTML = html;
+    // swiper 기능 ====================
+    const swVisual = new Swiper(".sw-visual", {
+      loop: true, // loop : 무한으로 도는 것.
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false, // 상관없이 계속 autoplay.
+      },
+      navigation: {
+        prevEl: ".visual-prev",
+        nextEl: ".visual-next",
+      },
+      pagination: {
+        // 하나하나 넘어가는 것.
+        el: ".visual-pg",
+        type: "fraction",
+      },
+    });
+    // 비주얼 슬라이드 멈춤 기능
+    const swVisualPlay = document.querySelector(".visual-play")
+    swVisualPlay.addEventListener("click" , function(){
+      if(swVisualPlay.classList.contains("active")){
+        swVisual.autoplay.start()
+        swVisualPlay.classList.remove("active")
+      }
+      else{
+        swVisual.autoplay.stop()
+        swVisualPlay.classList.add("active")
+      }
+    })
+  } // showVisual end ==============
+
+  //   ==========================end
 });
